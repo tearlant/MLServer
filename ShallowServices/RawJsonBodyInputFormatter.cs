ae3f3586a24285ae51918a71092f84663bca2dbe
tearlant <tearlacha@protonmail.com>
2024-03-05 21:51:30 -0500

    Add files missed in a previous commit

---
+++ ShallowServices/RawJsonBodyInputFormatter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace ShallowServices
+{
+    // Solution found at https://stackoverflow.com/questions/31952002/asp-net-core-v2-2015-mvc-how-to-get-raw-json-bound-to-a-string-without-a-typ
+    public class RawJsonBodyInputFormatter : InputFormatter
+    {
+        public RawJsonBodyInputFormatter()
+        {
+            this.SupportedMediaTypes.Add("application/json");
+        }
+
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+            var request = context.HttpContext.Request;
+            using (var reader = new StreamReader(request.Body))
+            {
+                var content = await reader.ReadToEndAsync();
+                return await InputFormatterResult.SuccessAsync(content);
+            }
+        }
+
+        protected override bool CanReadType(Type type)
+        {
+            return type == typeof(string);
+        }
+    }
+}
