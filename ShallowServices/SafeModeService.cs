c61e96a2d4e51bff7e475734dc0da1cde9870d20
tearlant <tearlacha@protonmail.com>
2024-03-22 17:32:12 -0400

    Add file that was missed in a previous commit

---
+++ ShallowServices/SafeModeService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace ShallowServices
+{
+    public class SecurityOptions
+    {
+        public bool SafeMode { get; set; }
+    }
+
+    public class SafeModeService
+    {
+        private readonly bool _isInSafeMode;
+
+        public SafeModeService(IOptions<SecurityOptions> securityOptions)
+        {
+            _isInSafeMode = securityOptions.Value.SafeMode;
+        }
+
+        public bool IsInSafeMode { get => _isInSafeMode; }
+
+    }
+}
