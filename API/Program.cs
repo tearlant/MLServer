a5b1fc53da0e9c0c893c542bacbb584549607065
tearlant <tearlacha@protonmail.com>
2024-03-28 13:02:23 -0400

    Ingesting certificate

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,57 @@
 using API.Extensions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Retrieve certificate paths from appsettings.json
+    var config = builder.Configuration;
+    var certPath = config["CertificatePaths:CertPath"];
+    var keyPath = config["CertificatePaths:KeyPath"];
+
+    var certPem = File.ReadAllText(certPath);
+    var keyPem = File.ReadAllText(keyPath);
+
+    // workaround: https://stackoverflow.com/questions/57739563/create-x509certificate2-from-crt-and-key-files-using-asp-net-core-3-0-built-in/75727256#75727256
+    var CreateCertFromPem = (string certString, string keyString) =>
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            return X509Certificate2.CreateFromPem(certString, keyString);
+
+        //workaround for windows issue https://github.com/dotnet/runtime/issues/23749#issuecomment-388231655
+        //using var cert = X509Certificate2.CreateFromPemFile(certString, keyString);
+        using var cert = X509Certificate2.CreateFromPem(certString, keyString);
+        return new X509Certificate2(cert.Export(X509ContentType.Pkcs12));
+    };
+
+    // workaround: https://stackoverflow.com/questions/57739563/create-x509certificate2-from-crt-and-key-files-using-asp-net-core-3-0-built-in/75727256#75727256
+    var CreateCertFromPemFile = (string certFile, string keyFile) =>
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            return X509Certificate2.CreateFromPemFile(certFile, keyFile);
+
+        //workaround for windows issue https://github.com/dotnet/runtime/issues/23749#issuecomment-388231655
+        //using var cert = X509Certificate2.CreateFromPemFile(certString, keyString);
+        using var cert = X509Certificate2.CreateFromPem(certFile, keyFile);
+        return new X509Certificate2(cert.Export(X509ContentType.Pkcs12));
+    };
+
+    var x509 = CreateCertFromPemFile(certPem, keyPem);
+
+    //var x509 = X509Certificate2.CreateFromPem(certPath, keyPath);
+
+    options.ListenAnyIP(8765, listenOptions =>
+    {
+        listenOptions.UseHttps(x509);
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
